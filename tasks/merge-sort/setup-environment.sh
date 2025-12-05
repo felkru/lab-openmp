3c15e35ffffb4d0bc4097577eae059054432e58d
felkru <acc@felkru.com>
2025-12-05 14:12:12 +0100

    Revert "parrallel memory for everything"
    This reverts commit e9a8522ee0fda11e35f9ae4be77c51eccb3d94e6.

--- tasks/merge-sort/setup-environment.sh
+++ tasks/merge-sort/setup-environment.sh
@@ -1,5 +1,2 @@
 export CXX=g++
 export FLAGS_OPENMP=-fopenmp
-export OMP_PROC_BIND=true
-# export OMP_PROC_BIND=close # alternative to OMP_PROC_BIND=true
-# export OMP_PLACES=cores # another alterative
